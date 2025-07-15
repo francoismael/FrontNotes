9d03eb6c452131da822627bea6f0d94fbb1aff98
franco <tolojanahary.francoismael@gmail.com>
2025-07-15 13:56:54 +0300

    [auth] use guard to secure page

--- src/app/notes/notes.routes.ts
+++ src/app/notes/notes.routes.ts
@@ -3,11 +3,12 @@ import { ListComponent } from "./list/list.component";
 import { CreateComponent } from "./create/create.component";
 import { DetailComponent } from "./detail/detail.component";
 import { EditComponent } from "./edit/edit.component";
+import { AuthGuard } from "../auth/auth.guard";
 
 export const NOTES_ROUTES: Routes = [
-  { path: '', component: ListComponent },
-  { path: 'create', component: CreateComponent},
-  { path: ':id', component: DetailComponent},
-  { path: ':id/delete', component: EditComponent},
-  { path: ':id/edit', component: EditComponent},
+  { path: '', component: ListComponent, canActivate: [AuthGuard] },
+  { path: 'create', component: CreateComponent, canActivate: [AuthGuard]},
+  { path: ':id', component: DetailComponent, canActivate: [AuthGuard]},
+  { path: ':id/delete', component: EditComponent, canActivate: [AuthGuard]},
+  { path: ':id/edit', component: EditComponent, canActivate: [AuthGuard]},
 ];
